bef0889427bb770075b746f8e14b978df56c2add
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2023-10-18 18:31:57 +0200

    modify index.html slideshow

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-window.onscroll = function() {scrollFunction()};
-document.addEventListener('DOMContentLoaded', (e)=> {
+window.onscroll = function () { scrollFunction() };
+document.addEventListener('DOMContentLoaded', (e) => {
     modalGallery();
+    slideShow();
 })
 
 
 const scrollFunction = () => {
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.querySelector('.navbar').style.background = 'rgba(244, 246, 247, 1)';
     } else {
         document.querySelector('.navbar').style.background = 'rgba(244, 246, 247, 0.3)';
@@ -15,16 +16,18 @@ const scrollFunction = () => {
 
 const modalGallery = () => {
     const images = document.querySelectorAll('.gallery-item img');
-    let imgSrc;
 
     images.forEach((img) => {
-        img.addEventListener('click', (e) => {
-            imgSrc = e.target.src;
-            imgModal(imgSrc);
-        });
+        img.addEventListener('click', () => {
+            imgModal(img);
+        })
     });
 
-    let imgModal = (src) =>{
+    let imgModal = (img) => {
+
+        console.log(img);
+        let src = img.src;
+
         const modal = document.createElement('div');
         modal.setAttribute('class', 'modal');
         document.querySelector('.gallery').append(modal);
@@ -39,5 +42,32 @@ const modalGallery = () => {
         }
 
         modal.append(newImage, closeBtn);
+    };
+}
+
+const slideShow = () => {
+    const slides = document.querySelectorAll('.slide');
+
+    let slideIndex = 0;
+
+    setInterval(() => {   
+        if(slideIndex === slides.length-1){
+            slideIndex = 0;
+        } else{
+            slideIndex++
+        };
+        changeSlide();
+    }, 2000);
+
+
+    const changeSlide = () => {
+        slides.forEach((slide) => {
+            if (slideIndex === slides.indexOf(slide)) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
     }
+ 
 }
